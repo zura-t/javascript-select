976a1043021deac9037a8d94137766c33e83cfb2
zura-t <66543460+zura-t@users.noreply.github.com>
2020-09-08 18:55:17 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import {Select} from './select/select'
+import './select/styles.scss'
+
+const select = new Select('#select', {
+	placeholder: 'выбрать элемент',
+	// selectedId: '4',
+	data: [
+		{id: '1', value: 'React'},
+		{id: '2', value: 'Angular'},
+		{id: '3', value: 'Vue'},
+		{id: '4', value: 'React Native'},
+		{id: '5', value: 'Next'},
+		{id: '6', value: 'Nest'}
+	],
+	onSelect(item) {
+		console.log('Selected Item', item);
+	}
+})
+
+window.s = select
